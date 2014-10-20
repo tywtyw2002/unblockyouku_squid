4004ff6654118e46f300a7f903de921625583d47
Landon@CosHiM <tywtyw2002@gmail.com>
2014-10-20 14:02:28 -0600

    Update sohu killer.

--- killerFlash/sohu.js
+++ killerFlash/sohu.js
@@ -1,93 +1,94 @@
 addKiller("sohu", {
 
-  "canKill": function(data) {
-    return /sohu\.com/.test(data.src);
-  },
-
-  A : function(module, exports, require, include) {
-    var _ = {};
-    //_.environment = "production";
-    //_.canPlayM3U8 = !!document.createElement("video").canPlayType("application/x-mpegURL") ? true : false;
-    _.noop = function() {};
-    _.each = function(arrOrObject, fn, context, i, len) {
-      if (typeof arrOrObject.length == "number") {
-        for (i = 0, len = arrOrObject.length; i < len; i++)
-          if (fn.call(context, arrOrObject[i], i) == false) break
-      } else
-        for (i in arrOrObject)
-          if (fn.call(context, arrOrObject[i], i) == false) break
-    };
-    _.extend = function(target) {
-      var length = arguments.length,
-        i = 1,
-        mixin;
-      while (i < length) {
-        mixin = arguments[i];
-        for (var name in mixin) {
-          if (mixin.hasOwnProperty(name)) {
-            target[name] = mixin[name]
-          }
-        }
-        i++
-      }
-      return target
-    };
-    _.queryString = function(query) {
-      var arr = [];
-      _.each(query, function(value, key) {
-        value !== undefined && arr.push(key + "=" + value)
-      });
-      return arr.join("&")
-    };
-    _.cookie = function(name) {
-      var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
-      if (arr != null) return unescape(arr[2]);
-      return null
-    };
-    _.byId = function(id) {
-      return document.getElementById(id)
-    };
-    _.rNode = function(el) {
-      try {
-        el.parentNode.removeChild(el)
-      } catch (e) {}
-    };
-    _.cTag = function(tagName, styles, attrs, out) {
-      var t = document.createElement(tagName),
-        cssText = "";
-      _.each(styles || {}, function(value, key) {
-        cssText += [key, ":", value, ";"].join("")
-      }), t.style.cssText = cssText;
-      _.each(attrs || {}, function(value, key) {
-        t.setAttribute(key, value)
-      });
-      return out && out.appendChild(t) || t
-    };
-    _.hasClass = function(el, cls) {
-      return el.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"))
-    };
-    _.addClass = function(el, cls) {
-      if (!_.hasClass(el, cls)) el.className += " " + cls
-    };
-    _.removeClass = function(el, cls) {
-      if (_.hasClass(el, cls)) {
-        el.className = el.className.replace(new RegExp("(\\s|^)" + cls + "(\\s|$)"), " ").trim()
-      }
-    };
-    _.jsonp = function(url, param, callback, handler) {
-      var scr, back = "HTML5PlayerBookMarkCodeByZythum" + +(new Date) + Math.random().toString().replace(".", "");
-      var hasQuery = url.indexOf("?") >= 0;
-      param = _.queryString(param);
-      if (param.length) param = (hasQuery ? "&" : "?") + param;
-      handler = [hasQuery || param.length ? "&" : "?", handler || "callback", "=", back].join("");
-      window[back] = function() {
-        callback && callback.apply(this, arguments);
-        delete window[back];
-        _.rNode(scr)
-      };
-      (scr = _.cTag("script", {}, {}, document.body)).src = url + param + handler
-    };
+    "canKill": function(data) {
+        return /sohu\.com/.test(data.src);
+    },
     /*
+    //A: function(module, exports, require, include) {
+    "A": function() {
+        var _ = {};
+        //_.environment = "production";
+        //_.canPlayM3U8 = !!document.createElement("video").canPlayType("application/x-mpegURL") ? true : false;
+        _.noop = function() {};
+        _.each = function(arrOrObject, fn, context, i, len) {
+            if (typeof arrOrObject.length == "number") {
+                for (i = 0, len = arrOrObject.length; i < len; i++)
+                    if (fn.call(context, arrOrObject[i], i) == false) break
+            } else
+                for (i in arrOrObject)
+                    if (fn.call(context, arrOrObject[i], i) == false) break
+        };
+        _.extend = function(target) {
+            var length = arguments.length,
+                i = 1,
+                mixin;
+            while (i < length) {
+                mixin = arguments[i];
+                for (var name in mixin) {
+                    if (mixin.hasOwnProperty(name)) {
+                        target[name] = mixin[name]
+                    }
+                }
+                i++
+            }
+            return target
+        };
+        _.queryString = function(query) {
+            var arr = [];
+            _.each(query, function(value, key) {
+                value !== undefined && arr.push(key + "=" + value)
+            });
+            return arr.join("&")
+        };
+        _.cookie = function(name) {
+            var arr = document.cookie.match(new RegExp("(^| )" + name + "=([^;]*)(;|$)"));
+            if (arr != null) return unescape(arr[2]);
+            return null
+        };
+        _.byId = function(id) {
+            return document.getElementById(id)
+        };
+        _.rNode = function(el) {
+            try {
+                el.parentNode.removeChild(el)
+            } catch (e) {}
+        };
+        _.cTag = function(tagName, styles, attrs, out) {
+            var t = document.createElement(tagName),
+                cssText = "";
+            _.each(styles || {}, function(value, key) {
+                cssText += [key, ":", value, ";"].join("")
+            }), t.style.cssText = cssText;
+            _.each(attrs || {}, function(value, key) {
+                t.setAttribute(key, value)
+            });
+            return out && out.appendChild(t) || t
+        };
+        _.hasClass = function(el, cls) {
+            return el.className.match(new RegExp("(\\s|^)" + cls + "(\\s|$)"))
+        };
+        _.addClass = function(el, cls) {
+            if (!_.hasClass(el, cls)) el.className += " " + cls
+        };
+        _.removeClass = function(el, cls) {
+            if (_.hasClass(el, cls)) {
+                el.className = el.className.replace(new RegExp("(\\s|^)" + cls + "(\\s|$)"), " ").trim()
+            }
+        };
+        _.jsonp = function(url, param, callback, handler) {
+            var scr, back = "HTML5PlayerBookMarkCodeByZythum" + +(new Date) + Math.random().toString().replace(".", "");
+            var hasQuery = url.indexOf("?") >= 0;
+            param = _.queryString(param);
+            if (param.length) param = (hasQuery ? "&" : "?") + param;
+            handler = [hasQuery || param.length ? "&" : "?", handler || "callback", "=", back].join("");
+            window[back] = function() {
+                callback && callback.apply(this, arguments);
+                delete window[back];
+                _.rNode(scr)
+            };
+            (scr = _.cTag("script", {}, {}, document.body)).src = url + param + handler
+        };
+        /*
     _.js = function(path, url) {
       url = _.environment == "development" ? "http://zythum.sinaapp.com:8080/youkuhtml5playerbookmark/client/development/js" : "http://2.zythum.sinaapp.com/youkuhtml5playerbookmark/client/production/js";
       return url + (path || "") + "?_t=" + +(new Date)
@@ -118,88 +119,150 @@ addKiller("sohu", {
       margin: "0"
     });
     */
-    _.log = function(text) {
-      window.console || window.console(text);
-      document.body.appendChild(_logNode).innerHTML = text
-    };
-    _.rlog = function() {
-      _.rNode(_logNode)
-    };
-    module.exports = _
-  },
+    /*
+        _.log = function(text) {
+            window.console || window.console(text);
+            document.body.appendChild(_logNode).innerHTML = text
+        };
+        _.rlog = function() {
+            _.rNode(_logNode)
+        };
+        module.exports = _
+    },
+    */
 
 
-  "process": function(data, callback) {
+    "process": function(data, s_callback) {
 
-    var flashvars = parseFlashVariables(data.params.flashvars);
-    var vid = flashvars.vid;
-    var uid = "1406271109096937";
-    //SUV 1406271109096937  .sohu.com / January 18, 2038 at 17:00:00  MST 19 B    
-    //var url = "http://tyw:x12345x@hot.proxy.c70.us:5204/ipad" + vid + ".m3u8";
-    var final_url;
+        var flashvars = parseFlashVariables(data.params.flashvars);
+        var vid = flashvars.vid;
+        var uid = "1406271109096937";
+        //SUV 1406271109096937  .sohu.com / January 18, 2038 at 17:00:00  MST 19 B    
+        //var url = "http://tyw:x12345x@hot.proxy.c70.us:5204/ipad" + vid + ".m3u8";
+        var final_url;
+        var _ = {};
 
 
-    function shift_en(i) {
-      var t = i.length,
-        e = 0;
-      return this.replace(/[0-9a-zA-Z]/g, function(s) {
-        var a = s.charCodeAt(0),
-          n = 65,
-          o = 26;
-        a >= 97 ? n = 97 : 65 > a && (n = 48, o = 10);
-        var r = a - n;
-        return String.fromCharCode((r + i[e++ % t]) % o + n)
-      })
-    }
+        _.each = function(arrOrObject, fn, context, i, len) {
+            if (typeof arrOrObject.length == "number") {
+                for (i = 0, len = arrOrObject.length; i < len; i++)
+                    if (fn.call(context, arrOrObject[i], i) == false) break
+            } else
+                for (i in arrOrObject)
+                    if (fn.call(context, arrOrObject[i], i) == false) break
+        };
 
-    function m3u8() {
-      var _ = require("A");
-      _.jsonp("http://proxy.c70.io:5204/pad.tv.sohu.com/playinfo", {
-        vid: vid,
-        playlistid: 0,
-        sig: shift_en.call("" + (new Date).getTime(), [23, 12, 131, 1321]),
-        key: shift_en.call(vid, [23, 12, 131, 1321]),
-        uid: uid
-      }, function(param) {
-        var url = "";
-        switch (param.quality) {
-          case 2:
-            url = param.norVid;
-            break;
-          case 1:
-            url = param.highVid;
-            break;
-          case 21:
-            url = param.superVid;
-            break;
-          case 31:
-            url = param.oriVid;
-            break;
-          default:
-            url = param.highVid
+        _.queryString = function(query) {
+            var arr = [];
+            _.each(query, function(value, key) {
+                value !== undefined && arr.push(key + "=" + value)
+            });
+            return arr.join("&")
+        };
+
+        _.rNode = function(el) {
+            try {
+                el.parentNode.removeChild(el)
+            } catch (e) {}
+        };
+
+        _.cTag = function(tagName, styles, attrs, out) {
+            var t = document.createElement(tagName),
+                cssText = "";
+            _.each(styles || {}, function(value, key) {
+                cssText += [key, ":", value, ";"].join("")
+            }), t.style.cssText = cssText;
+            _.each(attrs || {}, function(value, key) {
+                t.setAttribute(key, value)
+            });
+            return out && out.appendChild(t) || t
+        };
+
+        _.jsonp = function(url, param, callback, handler) {
+            var scr, back = "HTML5PlayerBookMarkCodeByZythum" + +(new Date) + Math.random().toString().replace(".", "");
+            var hasQuery = url.indexOf("?") >= 0;
+            param = _.queryString(param);
+            if (param.length) param = (hasQuery ? "&" : "?") + param;
+            handler = [hasQuery || param.length ? "&" : "?", handler || "callback", "=", back].join("");
+            window[back] = function() {
+                callback && callback.apply(this, arguments);
+                delete window[back];
+                _.rNode(scr)
+            };
+            (scr = _.cTag("script", {}, {}, document.body)).src = url + param + handler
+        };
+
+
+        function shift_en(i) {
+            safari.extension.globalPage.contentWindow.console.log(this);
+            var t = i.length,
+                e = 0;
+            return this.replace(/[0-9a-zA-Z]/g, function(s) {
+                var a = s.charCodeAt(0),
+                    n = 65,
+                    o = 26;
+                a >= 97 ? n = 97 : 65 > a && (n = 48, o = 10);
+                var r = a - n;
+                return String.fromCharCode((r + i[e++ % t]) % o + n)
+            })
         }
 
-        final_url = url.replace(/ipad\d+\_/, "ipad" + vid + "_").replace("http://", "http://proxy.c70.io:5204/") + "&uid=" + uid + "&ver=" + param.quality + "&prod=h5&pt=2&pg=1&ch=" + param.cid
-        return final_url;
-      })
+        function m3u8() {
+
+            _.jsonp("http://proxy.c70.io:5204/pad.tv.sohu.com/playinfo", {
+                vid: vid,
+                playlistid: 0,
+                sig: shift_en.call("" + (new Date).getTime(), [23, 12, 131, 1321]),
+                key: shift_en.call(vid, [23, 12, 131, 1321]),
+                uid: uid
+            }, function(param) {
+                var url = "";
+                switch (param.quality) {
+                    case 2:
+                        url = param.norVid;
+                        break;
+                    case 1:
+                        url = param.highVid;
+                        break;
+                    case 21:
+                        url = param.superVid;
+                        break;
+                    case 31:
+                        url = param.oriVid;
+                        break;
+                    default:
+                        url = param.highVid;
+                }
+
+                final_url = url.replace(/ipad\d+\_/, "ipad" + vid + "_").replace("http://", "http://proxy.c70.io:5204/") + "&uid=" + uid + "&ver=" + param.quality + "&prod=h5&pt=2&pg=1&ch=" + param.cid;
+                send_video(final_url);
+            })
+        }
+
+
+        function send_video(url) {
+            
+            safari.extension.globalPage.contentWindow.console.log(url);
+            //safari.extension.globalPage.contentWindow.console.log(flashvars);
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", data.location);
+            xhr.addEventListener("load", function() {
+
+                s_callback({
+                    "playlist": [{
+                        "poster": undefined,
+                        "sources": [{
+                            "url": decodeURIComponent(url),
+                            "isNative": true
+                        }]
+                    }]
+                });
+            }, false);
+            xhr.send();
+        }
+
+
+        m3u8();
+
     }
-
-    final_url = m3u8();
-    safari.extension.globalPage.contentWindow.console.log(flashvars);
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", data.location);
-    xhr.addEventListener("load", function() {
-
-      callback({
-        "playlist": [{
-          "poster": undefined,
-          "sources": [{
-            "url": decodeURIComponent(final_url),
-            "isNative": true
-          }]
-        }]
-      });
-    }, false);
-    xhr.send();
-  }
 });
